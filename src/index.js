d366b07b9aec3395587a7160b909d52534400478
satyamev <satyamev701600@gmail.com>
2024-05-30 12:39:21 +0530

    used cors and cookieParser

--- src/index.js
+++ src/index.js
@@ -8,3 +8,12 @@ dotenv.config({
 })
 
 connectDB()
+
+.then(() => {
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`server is on port: ${process.env.PORT}`);
+    });
+}) 
+.catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+})
