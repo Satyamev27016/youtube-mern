e7578b3489f7070184f900968864a3f062f2a7e5
satyamev <satyamev701600@gmail.com>
2024-06-13 12:34:10 +0530

    refactor: Update logoutUser function to clear refreshToken

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -161,11 +161,23 @@ const loginUser  = asyncHandler(async(req, res)=>{
 })
 
 const logoutUser = asyncHandler(async(req, res) => {
+      User.findByIdAndUpdate(
+         req.user._id,
+         {
+            $set:{
+               refreshToken: undefined
+            }
+         },
+         {
+            new: true
+         },
+         
       
+      )
 
 })
 
-
+// cookie 
 
 export  {
    registerUser,
